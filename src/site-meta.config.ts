57875c368bd3c827ac1c181e9c19377005525a6e
Paul Applegate <me@paulapplegate.com>
2022-08-28 01:51:00 +0000

    changes

--- src/site-meta.config.ts
+++ src/site-meta.config.ts
@@ -1,9 +1,9 @@
 const siteMeta = {
-  title: "Astro Theme Cactus",
-  description: "An opinionated starter theme for Astro",
-  lang: "en-GB",
-  ogLocale: "en_GB",
-  githubUrl: "https://github.com/chrismwilliams/astro-theme-cactus",
+  title: "Living Life",
+  description: "Follow my daily hijinks here. A lot of science-related stuff, and some serious subjects like cancer. Fuck cancer.",
+  lang: "en-US",
+  ogLocale: "en_US",
+  githubUrl: "https://github.com/theapplegates/cactus-templateAug27",
   themeColorLight: "#fafafa",
   themeColorDark: "#1d1f21",
 }
